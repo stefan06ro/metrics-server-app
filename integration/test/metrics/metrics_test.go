6767f851de0633cf2251402dd444c944117b4a89
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-10-30 18:19:55 +0100

    Use apptest library and refactor tests (#34)
    * Update go modules
    
    * Remove basic test
    
    * Update metrics test to use apptest
    
    * Use kubeconfig path
    
    * Fix exit code handling
    
    * Use apptest branch
    
    * Actually run the test
    
    * Use apptest v0.4.1
    
    * Use apptestctl v0.4.0

--- integration/test/metrics/metrics_test.go
+++ integration/test/metrics/metrics_test.go
@@ -9,48 +9,41 @@ import (
 
 	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
-	longMaxInterval    = 2 * time.Minute
 	metricsAPIEndpoint = "/apis/metrics.k8s.io/v1beta1"
-	shortMaxInterval   = 5 * time.Second
 )
 
 // TestMetrics ensures that deployed metrics-server chart exposes node-metrics
 // via Kubernetes API extension.
 func TestMetrics(t *testing.T) {
 	ctx := context.Background()
+	var err error
 
-	// Install chart and wait for deployed status
-	err := ba.Test(ctx)
-	if err != nil {
-		t.Fatalf("%#v", err)
-	}
-
-	// Check metrics availability
-	err = checkMetricsAvailability(ctx)
+	// Check metrics-server deployment is ready.
+	err = checkReadyDeployment(ctx)
 	if err != nil {
 		t.Fatalf("could not get metrics: %v", err)
 	}
 
-	// Delete release
-	err = helmClient.DeleteRelease(ctx, metav1.NamespaceSystem, appName)
+	// Check metrics availability.
+	err = checkMetricsAvailability(ctx)
 	if err != nil {
-		t.Fatalf("failed to teardown resource: %v", err)
+		t.Fatalf("could not get metrics: %v", err)
 	}
 }
 
 func checkMetricsAvailability(ctx context.Context) error {
 	var err error
 
-	restClient := k8sClients.K8sClient().CoreV1().RESTClient()
+	restClient := appTest.K8sClient().CoreV1().RESTClient()
 
 	l.LogCtx(ctx, "level", "debug", "message", "waiting for the metrics become available")
 
 	o := func() error {
-
 		_, err := restClient.Get().RequestURI(metricsAPIEndpoint).Stream(context.TODO())
 		if err != nil {
 			return err
@@ -58,7 +51,7 @@ func checkMetricsAvailability(ctx context.Context) error {
 
 		return nil
 	}
-	b := backoff.NewConstant(longMaxInterval, shortMaxInterval)
+	b := backoff.NewConstant(2*time.Minute, 5*time.Second)
 	n := backoff.NewNotifier(l, ctx)
 
 	err = backoff.RetryNotify(o, b, n)
@@ -67,6 +60,38 @@ func checkMetricsAvailability(ctx context.Context) error {
 	}
 
 	l.LogCtx(ctx, "level", "debug", "message", "successfully retrieved metrics from metrics server")
+
+	return nil
+}
+
+func checkReadyDeployment(ctx context.Context) error {
+	var err error
+
+	l.LogCtx(ctx, "level", "debug", "message", "waiting for ready deployment")
+
+	o := func() error {
+		deploy, err := appTest.K8sClient().AppsV1().Deployments(metav1.NamespaceSystem).Get(ctx, app, metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			return microerror.Maskf(executionFailedError, "deployment %#q in %#q not found", app, metav1.NamespaceSystem)
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		if deploy.Status.ReadyReplicas != *deploy.Spec.Replicas {
+			return microerror.Maskf(executionFailedError, "deployment %#q want %d replicas %d ready", app, *deploy.Spec.Replicas, deploy.Status.ReadyReplicas)
+		}
+
+		return nil
+	}
+	b := backoff.NewConstant(2*time.Minute, 5*time.Second)
+	n := backoff.NewNotifier(l, ctx)
+
+	err = backoff.RetryNotify(o, b, n)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	l.LogCtx(ctx, "level", "debug", "message", "deployment is ready")
 
 	return nil
 }
