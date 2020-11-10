098a0482120b979b46338db5a44a7f8cc79a6484
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-11-10 14:54:39 +0100

    Fix error.go for integration test (#38)

--- integration/test/metrics/error.go
+++ integration/test/metrics/error.go
@@ -1,7 +1,9 @@
+// +build k8srequired
+
 package metrics
 
 import "github.com/giantswarm/microerror"
 
 var executionFailedError = &microerror.Error{
-	Kind: "executiomFailedError",
+	Kind: "executionFailedError",
 }
